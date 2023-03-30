e825c549f869f4f43e5ab278b7649498065eb84c
gastonloco <ferreyragaston351@gmail.com>
2023-03-30 14:40:18 -0300

    ocultar presupuesto

--- js/script.js
+++ js/script.js
@@ -1,4 +1,21 @@
-fetch('https://jsonplaceholder.typicode.com/todos')
-      .then(response => response.json())
-      .then(json => console.log(json))
-      console.log(document)
+window.onload = ocultarFormulario;
+
+function ocultarFormulario() {
+      var formulario = document.getElementById("miFormulario-1");
+      var btnOcultar = document.getElementById("btn-2-ocultar");
+      var btnMostrar = document.getElementById("btn-2-mostrar");
+      formulario.style.display = "none";
+      btnOcultar.style.display = "none";
+      btnMostrar.style.display = "initial";
+}
+
+
+function mostrarFormulario() {
+      var formulario = document.getElementById("miFormulario-1");
+      var btnOcultar = document.getElementById("btn-2-ocultar");
+      var btnMostrar = document.getElementById("btn-2-mostrar");
+
+      formulario.style.display = "initial";
+      btnOcultar.style.display = "initial";
+      btnMostrar.style.display = "none";
+}
