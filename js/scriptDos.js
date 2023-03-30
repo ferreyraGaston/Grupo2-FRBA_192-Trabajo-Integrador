9500134dbbdfb7a16769e66237cc9baa8ea23e2b
david.sot <davidsoto_2@hotmail.com>
2023-03-30 15:04:15 -0300

    list servicios

---
+++ js/scriptDos.js
@@ -0,0 +1,16 @@
+const contenedorHTML = document.getElementById("contenedor")
+
+fetch("./database.json")
+    .then(Response => Response.json())
+    .then(data =>{
+        for(const servicio of data){
+            contenedorHTML.innerHTML+=`
+            <div class= "card">
+                <h2>${servicio.name}</h2>
+                <img src=${servicio.url}
+                <h3>${servicio.servicio}
+                <h4><strong>${servicio.costo}<strong></h4>
+            </div>
+            `
+        }
+})
